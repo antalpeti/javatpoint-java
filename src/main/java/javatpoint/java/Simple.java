111f88daad514ab310e9c736565505c687f61941
antalpeti <antalpeti@users.noreply.github.com>
2024-09-13 03:18:48 +0200

    modify groupId; create Simple class

---
+++ src/main/java/javatpoint/java/Simple.java
@@ -0,0 +1,12 @@
+package javatpoint.java;
+
+import java.util.logging.Logger;
+
+class Simple {
+
+  private static final Logger logger = Logger.getLogger(Simple.class.getName());
+
+  public static void main(String[] args) {
+    logger.info("Hello Java");
+  }
+}
